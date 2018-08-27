1a0ae5f17257dcef76ce43964aae97fe6fc5cf77
jkstahl <neobaud@gmail.com>
2018-08-26 22:33:24 -0500

    Added save/restore and NB.  pca didn't work.

--- ai_model2.py
+++ ai_model2.py
@@ -1,19 +1,23 @@
 import pandas as pd
 import numpy as np
 import nflgame
-import math
+import math, pickle, os
 from statistical_model import get_team
 from calendar import week
 from statistical_model import home_model as hm
 
+from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
 
 START_YEAR = 2009
 MAX_WEEKS = 17
 MIN_YEAR = 2009
 MIN_W = 06
 MIN_WEEK = MIN_YEAR * 100 + MIN_W  # sixth week of 2009
-
+SAVE_FILENAME = 'savestats.dat'
 
 class new_model():
     def __init__(self):
@@ -33,94 +37,117 @@ class new_model():
             print (output)
     
     def load_stats(self):
-        stats_map = {}
-        # build all of the stuff in the stats map
-        remove = set(['pos_time'])
-        self.outputs = {'week': [], 'output': []}
-        
-        reg_attribs = ['win', 'team', 'id', 'home', 'date', 'week', 'score']
-        extra_attribs = ['rushing_att', 'passing_att']
-        
-        for att in extra_attribs:
-            stats_map[att] = []
-            # defense
-            stats_map['d' + att] = []
-        
-        for att in reg_attribs:
-            stats_map[att] = []
-        
-        self.stats =  list(set(nflgame.games(MIN_YEAR, 1)[0].stats_home._fields) - remove)
-        self.print_out( self.stats)
-        d_stats = []
-        for stat in self.stats:
-            stats_map[stat] = []
-            stats_map['d' + stat] = []
-            d_stats.append('d' + stat)
-        
-        
-        game_num = 0
         self.last_year, self.last_week = nflgame.live.current_year_and_week() 
-        for year in range(START_YEAR, self.last_year+1):
-            for week in range(1, self.last_week + 1):
-                self.print_out ('Year %d, week %d' % (year, week))
-                games = nflgame.games(year, week)
-                for game in games:
-                    play_week = game.schedule['year'] * 100 + game.schedule['week']
-                    self.outputs['week'].append(play_week)
-                    self.outputs['output'].append(float(get_team(game.winner) == get_team(game.home)))
-                    
-                    teams = [get_team(game.home), get_team(game.away)]
-                    for teamn, team in enumerate(teams):
-                        stats_map['win'].append(float(get_team(game.winner) == get_team(team)))
-                        stats_map['id'].append(game_num)
-                        stats_map['home'].append((teamn + 1) % 2)
-                        stats_map['team'].append(get_team(team))
-                        stats_map['date'].append(int(game.schedule['eid']) / 100)
-                        stats_map['week'].append(play_week)
-                    
-                    stats_map['score'].append(game.score_home)
-                    stats_map['score'].append(game.score_away)
-                    
-                    for stat in self.stats:
-                        # offense
-                        stats_map[stat].append(float(game.stats_home.__getattribute__(stat)))
-                        stats_map[stat].append(float(game.stats_away.__getattribute__(stat)))
+        if os.path.exists(SAVE_FILENAME):
+            self.print_out('Loading old stat data..')
+            self.stats, self.stat_data, self.outputs, last_year, last_week = pickle.load(open(SAVE_FILENAME))
+        
+        if not os.path.exists(SAVE_FILENAME) or last_year != self.last_year or last_week != self.last_week:
+            self.print_out('Savefile not found.  Loading new stats.')
+            stats_map = {}
+            # build all of the stuff in the stats map
+            remove = set(['pos_time'])
+            self.outputs = {'week': [], 'output': []}
+            
+            reg_attribs = ['win', 'team', 'id', 'home', 'date', 'week', 'score']
+            extra_attribs = ['rushing_att', 'passing_att']
+            
+            for att in extra_attribs:
+                stats_map[att] = []
+                # defense
+                stats_map['d' + att] = []
+            
+            for att in reg_attribs:
+                stats_map[att] = []
+            
+            self.stats =  list(set(nflgame.games(MIN_YEAR, 1)[0].stats_home._fields) - remove)
+            self.print_out( self.stats)
+            d_stats = []
+            for stat in self.stats:
+                stats_map[stat] = []
+                stats_map['d' + stat] = []
+                d_stats.append('d' + stat)
+            
+            
+            game_num = 0
+            
+            for year in range(START_YEAR, self.last_year+1):
+                for week in range(1, self.last_week + 1):
+                    self.print_out ('Year %d, week %d' % (year, week))
+                    games = nflgame.games(year, week)
+                    for game in games:
+                        play_week = game.schedule['year'] * 100 + game.schedule['week']
+                        self.outputs['week'].append(play_week)
+                        self.outputs['output'].append(float(get_team(game.winner) == get_team(game.home)))
                         
-                        # swap order of home away for defense
-                        stats_map['d' + stat].append(float(game.stats_away.__getattribute__(stat)))
-                        stats_map['d' + stat].append(float(game.stats_home.__getattribute__(stat)))
+                        teams = [get_team(game.home), get_team(game.away)]
+                        for teamn, team in enumerate(teams):
+                            stats_map['win'].append(float(get_team(game.winner) == get_team(team)))
+                            stats_map['id'].append(game_num)
+                            stats_map['home'].append((teamn + 1) % 2)
+                            stats_map['team'].append(get_team(team))
+                            stats_map['date'].append(int(game.schedule['eid']) / 100)
+                            stats_map['week'].append(play_week)
                         
-                    
-                    # for home and away get extra stats
-                    for h in [True, False]:
-                        new_stats = self.__get_weird_stats__(game, extra_attribs, h)
-                        for stat, value in zip(extra_attribs, new_stats ):
-                            stats_map[stat].append(value)
+                        stats_map['score'].append(game.score_home)
+                        stats_map['score'].append(game.score_away)
+                        
+                        for stat in self.stats:
+                            # offense
+                            stats_map[stat].append(float(game.stats_home.__getattribute__(stat)))
+                            stats_map[stat].append(float(game.stats_away.__getattribute__(stat)))
                             
-                            # swap for defense
-                            if not h:
-                                stats_map['d' + stat].insert(-1, value)
-                            else:
-                                 stats_map['d' + stat].append(value)
-                                 
-                    game_num +=1
-                    
-        self.stats.append('win')
-        self.stats.append('score')
-        self.stat_data = pd.DataFrame(stats_map)     
-        self.outputs = pd.DataFrame(self.outputs)
-        
-        # transform 
-        for d in ['', 'd']:
-            self.stat_data[d + 'rush_per_att'] = self.stat_data[d + 'rushing_yds'] / self.stat_data[d + 'rushing_att']
-            self.stat_data[d + 'pass_per_att'] = self.stat_data[d + 'passing_yds'] / self.stat_data[d + 'passing_att']
-        
-        # drop colls
-        self.stats += (['rush_per_att', 'pass_per_att'] + ['drush_per_att', 'dpass_per_att'] + d_stats)
-        drop_list = ['rushing_att', 'passing_yds', 'passing_att']
-        self.stats = list(set(self.stats) - set(drop_list))
-        
+                            # swap order of home away for defense
+                            stats_map['d' + stat].append(float(game.stats_away.__getattribute__(stat)))
+                            stats_map['d' + stat].append(float(game.stats_home.__getattribute__(stat)))
+                            
+                        
+                        # for home and away get extra stats
+                        for h in [True, False]:
+                            new_stats = self.__get_weird_stats__(game, extra_attribs, h)
+                            for stat, value in zip(extra_attribs, new_stats ):
+                                stats_map[stat].append(value)
+                                
+                                # swap for defense
+                                if not h:
+                                    stats_map['d' + stat].insert(-1, value)
+                                else:
+                                    stats_map['d' + stat].append(value)
+                                     
+                        game_num +=1
+                        
+            self.stats.append('win')
+            self.stats.append('score')
+            self.stat_data = pd.DataFrame(stats_map)     
+            self.outputs = pd.DataFrame(self.outputs)
+            
+            # transform 
+            for d in ['', 'd']:
+                self.stat_data[d + 'rush_per_att'] = self.stat_data[d + 'rushing_yds'] / self.stat_data[d + 'rushing_att']
+                self.stat_data[d + 'pass_per_att'] = self.stat_data[d + 'passing_yds'] / self.stat_data[d + 'passing_att']
+            
+            # drop colls
+            self.stats += (['rush_per_att', 'pass_per_att'] + ['drush_per_att', 'dpass_per_att'] + d_stats)
+            drop_list = ['rushing_att', 'passing_yds', 'passing_att']
+            self.stats = list(set(self.stats) - set(drop_list))
+            
+            
+            self.print_out('Saving data...')
+            pickle.dump((self.stats, self.stat_data, self.outputs, self.last_year, self.last_week), open(SAVE_FILENAME, 'w'))
+            
         self.old_stats = self.stats
+        
+    
+    def __remove_uncorrelated__(self, df, corr_val):
+        usable = []
+        win_correlation = df.corr()['win']
+        self.print_out (win_correlation)
+        
+        for col, val in win_correlation.iteritems():
+            if abs(val) >= .2:
+                self.print_out('Removing %s' % col)
+                usable.append(col)
+        return usable
     
     def process_records(self, alpha= .5):
         #yw = year * 100 + week
@@ -135,26 +162,30 @@ class new_model():
             recs.update(team_time_series)
             pass
         
-        self.recs = recs
-
         self.print_out('Correlations:')
-        win_correlation = self.recs.corr()['win']
-        self.print_out (win_correlation)
-        
-        for col, val in win_correlation.iteritems():
-            if abs(val) < .2 and col in self.stats:
-                self.print_out('Removing %s' % col)
-                self.stats.remove(col)
-        
+        self.stats = self.__remove_uncorrelated__(recs, .2)
+
         self.print_out('Using columns: %s' % str(self.stats))
-        self.print_out(self.recs[['win', 'team']][-32:])
+        self.print_out(recs[['win', 'team']][-32:])
+        '''
+        # Do pca and reselect
+        self.pca = PCA()
+        fit = self.pca.fit_transform(recs[self.stats].values)
+        fit_df = pd.DataFrame(data=fit, index=range(fit.shape[0]), columns=range(fit.shape[1]))
+        fit_df['win'] = recs['win']
+        self.stats = self.__remove_uncorrelated__(fit_df, .2)
+        #self.stats.remove('win')
+        recs = pd.concat([recs, fit_df.drop('win', axis=1)], axis = 1)
+        '''
+        self.recs = recs
+        
         # get home and away
         home_teams = recs[recs['home'] == 1]
         away_teams = recs[recs['home'] == 0]
         
         # combine home and away
-        self.game_recs = pd.merge(home_teams[['id','week'] + self.stats], away_teams[['id'] + self.stats], on='id').drop(['id'], axis=1)
-
+        self.game_recs = pd.merge(home_teams[self.stats + ['week', 'id']], away_teams[self.stats + ['id']], on='id').drop(['id'], axis=1)
+        pass
         
     
     def train(self, year, week):
@@ -166,14 +197,16 @@ class new_model():
         outputs = self.outputs[(self.outputs['week'] <= yw) & (self.outputs['week'] >= MIN_WEEK)].drop('week', axis=1)
         assert len(valid_recs) == len(outputs)
         
-        self.model = RandomForestClassifier(max_depth=3)
+        #self.model = RandomForestClassifier(max_depth=5)
+        self.model = GaussianNB() 
         self.model.fit(valid_recs.values, outputs.values)
-        pass
+        
     
     def predict(self, home_team, away_team):
         home = self.recs[self.recs['team'] == home_team].iloc[-1][self.stats]
         away = self.recs[self.recs['team'] == away_team].iloc[-1][self.stats]
-        
+        self.home_pca.transform(home.values)
+        self.away_pca.transform(away.values)
         return self.model.predict(home.append(away).to_frame().values.T)
     
     def predict_week(self, year, week):
@@ -186,7 +219,7 @@ class new_model():
         game_set = self.outputs[self.outputs['week'] == yw].drop('week', axis=1)
         return game_set.values
     
-    def back_test(self):
+    def back_test(self, reruns = 1):
         psum = 0
         i = 0
         self.print_out('Running backtest...')
@@ -197,11 +230,15 @@ class new_model():
                 i += 1
                 if year == MIN_YEAR and week < MIN_W or week == MAX_WEEKS and year == self.last_year:
                     continue
-                self.train(year, week)
-                next_year, next_week = (year, week + 1) if week < MAX_WEEKS else (year + 1, week)
-                predict = self.predict_week(next_year, next_week)
-                actual = self.actual_week(next_year, next_week)
-                acuracy =1.0 * np.sum(predict == actual.T) / len(predict)
+                rerun_sum = 0
+                for run in range(reruns):
+                    self.train(year, week)
+                    next_year, next_week = (year, week + 1) if week < MAX_WEEKS else (year + 1, week)
+                    predict = self.predict_week(next_year, next_week)
+                    actual = self.actual_week(next_year, next_week)
+                    acuracy =1.0 * np.sum(predict == actual.T) / len(predict)
+                    rerun_sum += acuracy
+                acuracy = rerun_sum / reruns
                 psum += acuracy
                 year_acc += acuracy
                 #self.print_out('Year %d, week %d: %f' % (year, week, acuracy))
