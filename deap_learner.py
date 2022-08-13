16eb33b37a38c99e8cb4e8cd5fb73bf9b5cc258d
jkstahl <neobaud@gmail.com>
2022-08-13 13:54:17 -0500

    Initial commit

---
+++ deap_learner.py
@@ -0,0 +1,132 @@
+
+import pandas as pd
+import numpy as np
+
+import math, pickle, os
+from statistical_model import get_team
+from calendar import week
+import multiprocessing as mp
+from game_scraper2 import game_scraper
+
+START_YEAR = 2009
+MAX_WEEKS = 17
+MIN_YEAR = 2009
+MIN_W = 6
+MIN_WEEK = MIN_YEAR * 100 + MIN_W  # sixth week of 2009
+SAVE_FILENAME = 'savestats.dat'
+
+
+
+class deap_learner_model():
+    stat_data = None
+    stats = None
+    outputs = None
+    
+    def __init__(self):
+        self.raw_stats = None
+        self.print_it = True
+        self.stats = game_scraper(2019).stats()
+        pass
+        self.transform()
+        self.make_vectors()
+    
+    def __windowed_average__(self, x):
+        window = self.pre_compute[len(self.pre_compute)-len(x):] 
+        return np.sum(window * x) / window.sum()    
+    
+    def transform(self):
+        alpha = .05
+        
+        self.pre_compute = np.fliplr([np.exp(np.array(range((1000))) * np.log(1-alpha))])[0]
+        transformers = [
+                            'extra_points_attempted',
+                            'extra_points_made',
+                            'field_goals_attempted',
+                            'field_goals_made',
+                            'fourth_down_attempts', 
+                            'fourth_down_conversions', 
+                            'interceptions',
+                            'pass_attempts', 
+                            'pass_completion_rate', 
+                            'pass_completions',
+                            'pass_touchdowns', 
+                            'pass_yards', 
+                            'pass_yards_per_attempt',
+                            'points_allowed', 
+                            'points_scored',
+                            'punt_yards', 
+                            'punts',
+                            'quarterback_rating', 
+                            'rush_attempts', 
+                            'rush_touchdowns',
+                            'rush_yards', 
+                            'rush_yards_per_attempt', 
+                            'third_down_attempts',
+                            'third_down_conversions', 
+                            'times_sacked',
+                            'yards_lost_from_sacks',
+                            'result',
+                            ]
+        others = {    
+                            'result' : lambda x : 1 if x == 'Win' else 0,
+                            'location'  : lambda x : 1 if x == 'Home' else 0,
+                            'week' : lambda x : x / 17,
+                    } 
+        for team, df in self.stats.items():
+            for o, f in others.items():
+                df[o] = df[o].apply(f)
+            df['output'] = df['result']
+        for team, df in self.stats.items():
+            for col in transformers:
+                df[col] = df[col].expanding().apply(self.__windowed_average__, raw=True)
+        pass
+    
+    def make_vectors(self):
+        # stack all of the teams
+        self.vects = pd.DataFrame()
+        for team, df in self.stats.items():
+            self.vects = self.vects.append(df,)
+        
+        use_columns =      ['extra_points_attempted',
+                            'extra_points_made',
+                            'field_goals_attempted',
+                            'field_goals_made',
+                            'fourth_down_attempts', 
+                            'fourth_down_conversions', 
+                            'interceptions',
+                            'pass_attempts', 
+                            'pass_completion_rate', 
+                            'pass_completions',
+                            'pass_touchdowns', 
+                            'pass_yards', 
+                            'pass_yards_per_attempt',
+                            'points_allowed', 
+                            'points_scored',
+                            'punt_yards', 
+                            'punts',
+                            'quarterback_rating', 
+                            'rush_attempts', 
+                            'rush_touchdowns',
+                            'rush_yards', 
+                            'rush_yards_per_attempt', 
+                            'third_down_attempts',
+                            'third_down_conversions', 
+                            'times_sacked',
+                            'yards_lost_from_sacks',
+                            'result',
+                            'week',
+                            'location',
+                            'output']
+        self.vects = self.vects[use_columns]
+        
+        self.vects = self.vects.loc[self.vects['location'] == 1].join(self.vects.loc[self.vects['location'] == 0], rsuffix='_away')
+        # pull out home and away games
+        self.outputs = self.vects['output']
+        self.vects.drop(['output','output_away', 'location', 'location_away'], axis=1, inplace=True)
+        
+        pass
+
+if __name__ == '__main__':  
+    dl = deap_learner_model()
+    print (dl.stats['SEA'].columns)
+    pass
---
+++ game_scraper2.py
@@ -0,0 +1,87 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+from sportsreference.nfl.schedule import Schedule 
+from sportsreference.nfl.teams import Teams
+from datetime import datetime 
+import multiprocessing as mp
+import os, pickle, shelve
+lock = mp.Lock()
+lock_load = mp.Lock()
+START_YEAR = 2018
+
+class Game:
+            def __init__(self, game):
+                self.keys = game.keys()
+                self.__dict__.update( game)
+            
+            def __repr__(self):
+                return str(self.__dict__)
+            
+class game_scraper():
+    reload = False
+    
+    def __init__(self, to_year= 2019):
+        self.cache_filename = self.__class__.__name__ + '.db'
+
+        self.load()
+        if game_scraper.reload:
+            self.clear()
+        
+        self._teams = self.cache['teams'] if 'teams' in self.cache else set([])
+        self._stats = self.cache['stats']
+        self._cached_tuples = self.cache['_cached_tuples']
+        last_year = self.cache['last_year']
+
+        #print ('last year ' + str(last_year))
+        
+        for year in range(START_YEAR, to_year + 1):
+            if year in self._cached_tuples and not game_scraper.reload:
+                continue
+            self._cached_tuples.add(year)
+            print (year)
+            year_done = True
+            for team in Teams(year):
+                self._teams.add(team.abbreviation)
+                s = Schedule(team.abbreviation, year)
+                print (s.dataframe)
+                if team.abbreviation in self._stats:
+                        self._stats[team.abbreviation] = self._stats[team.abbreviation].append(s.dataframe)
+
+                else:
+                    self._stats[team.abbreviation] = s.dataframe
+                
+            if year_done:
+                self.cache['last_year'] = year
+        
+        print (self._stats)
+        
+        #print (self._games[(2020,1)])
+        self.teams = [[team] for team in self._teams]
+        #if self._games != self.cache['games']:
+        self.cache['stats'] = self._stats
+        self.cache['teams'] = self._teams
+        self.cache['_cached_tuples'] = self._cached_tuples
+        self.cache.sync()
+        
+        print(str(self.teams))
+        assert(len(self.teams) == 32)
+        game_scraper.reload=False
+    
+    def clear(self):
+        self.cache['stats'] = {}
+        self.cache['last_year'] = 0
+        self.cache['teams'] = set([])
+        self.cache['_cached_tuples'] = set([])
+    
+    
+    def load(self):
+        self.cache = shelve.open(self.cache_filename, writeback = True)
+
+
+    def stats(self):
+        return self._stats
+        
+
+if __name__ == '__main__':  
+    gs = game_scraper()
